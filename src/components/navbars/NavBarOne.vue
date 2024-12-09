b2d2b9ef21f74729df558c6df41a31ad90fa147c
anquitas <learningcurve2048@gmail.com>
2024-12-09 15:37:36 +0300

    first commit

---
+++ src/components/navbars/NavBarOne.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="navbar bg-base-100">
+  <a class="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavBarOne'
+}
+</script>
+
+<style scoped>
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+} 
+</style>
