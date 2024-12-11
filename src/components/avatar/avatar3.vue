2a37c2d4eee16cd07c8eefae59e4e4b412eb7fbd
serpil04 <114863643+serpil04@users.noreply.github.com>
2024-12-11 15:17:16 +0200

    accordion, avatar -serp

---
+++ src/components/avatar/avatar3.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="avatar">
+  <div class="w-24 rounded-xl">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+</div>
+<div class="avatar">
+  <div class="w-24 rounded-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name:'avatar3'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/avatar/avatar8.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="avatar online">
+  <div class="w-24 rounded-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+</div>
+<div class="avatar offline">
+  <div class="w-24 rounded-full">
+    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+name:'avatar8'
+}
+</script>
+
+<style>
+
+</style>
