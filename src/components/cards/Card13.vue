19d270aafb55d2eb0c9d085ad6ba3bb5fbffb6a6
nurefsanbozkurt <bzkrt.nurefsan@gmail.com>
2024-12-11 15:40:54 +0300

    cards, stats -nfs

---
+++ src/components/cards/Card13.vue
@@ -0,0 +1,28 @@
+<!-- Responsive card (vertical on small screen, horizontal on large screen) -->
+
+<template>
+ <div class="card lg:card-side bg-base-100 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.webp"
+      alt="Album" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">New album is released!</h2>
+    <p>Click the button to listen on Spotiwhy app.</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Listen</button>
+    </div>
+  </div>
+</div> 
+</template>
+
+<script>
+export default {
+name: 'Card13'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/cards/Card2.vue
@@ -0,0 +1,28 @@
+<!-- Compact card (less padding for `card-body`) -->
+
+<template>
+ <div class="card card-compact bg-base-100 w-96 shadow-xl">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div> 
+</template>
+
+<script>
+export default {
+name: 'Card2'
+}
+</script>
+
+<style>
+
+</style>
